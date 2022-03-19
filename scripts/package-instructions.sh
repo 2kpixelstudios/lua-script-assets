2a07cdd2ca29074e75401d70d62141cd84e1b4e5
Carlos DaLomba <carlos@cometpeakgames.net>
2022-03-18 23:21:16 -0400

    Added 1st iteration of docker files from unity-project-packager / aseprite-importer

---
+++ scripts/package-instructions.sh
@@ -0,0 +1,11 @@
+repoFolder="/src" # TODO: Make this compatible running outside of Docker again...
+version="v0.1.0"
+firstVersion="v0.1.0"
+copyFiles=(
+    "README.md"
+    "LICENSE.md"
+    "CHANGELOG.md"
+    "package.json"
+)
+unityPackageFile="package.json"
+pushChanges=true
