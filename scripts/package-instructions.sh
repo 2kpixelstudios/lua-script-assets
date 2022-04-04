6383e6a9ddef265c06cbc154dfb415d6e1055232
Carlos DaLomba <carlos@cometpeakgames.net>
2022-04-04 14:04:16 -0400

    Added .gitignore to package-instructions.sh

--- scripts/package-instructions.sh
+++ scripts/package-instructions.sh
@@ -2,6 +2,7 @@ repoFolder="/src" # TODO: Make this compatible running outside of Docker again..
 version="v0.1.0"
 firstVersion="v0.1.0"
 copyFiles=( # NOTE: Corresponding meta files will automatically be checked for and copied over if possible
+    ".gitignore"
     "README.md"
     "LICENSE.md"
     "CHANGELOG.md"
