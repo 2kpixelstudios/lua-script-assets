ac41f67a21559493582e4de271c6d5b2beda2d8b
Carlos DaLomba <carlos@cometpeakgames.net>
2022-04-05 09:35:39 -0400

    Updated packager to new format (see unity-project-packager)
    - Updated package instructions.sh
    - Added new bind mount to docker-compose.packager.yml
    - Added .env to gitignore

--- scripts/packaging/instructions.sh
+++ scripts/packaging/instructions.sh
@@ -1,6 +1,5 @@
 repoFolder="/src" # TODO: Make this compatible running outside of Docker again...
-version="v0.1.0"
-firstVersion="v0.1.0"
+version="v0.1.1"
 copyFiles=( # NOTE: Corresponding meta files will automatically be checked for and copied over if possible
     ".gitignore"
     "README.md"
